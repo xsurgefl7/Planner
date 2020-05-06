e66df2aff1503660080efe39aae777f7d80cdea5
xsurgelf7 <sergiozy6693@gmail.com>
2020-05-06 15:03:10 -0500

    Added Priority and Multiple Events

--- Planner-rework/src/Planner/Date.java
+++ Planner-rework/src/Planner/Date.java
@@ -14,8 +14,9 @@ String inputString;
 String Inputdays;
 int version=0;
 int version2=0;
-String Title,Location,InputNum,update;
-int Days;
+int EventIncr = 0;
+String InputNum;
+int Days =0;
 int Modulo;
 int EventNum;
 int ArrayNum = 30;
@@ -26,16 +27,14 @@ LocalDate ReminderY;
 String ReminderDin = "null", ReminderWin = "null",ReminderMin = "null",ReminderYin = "null",Titlein = "null",Localbot = "null";
 String[] EventUpdated = new String[ArrayNum]; //Array Holding Each Event Date, title and location by version 
 String[] ReminderUpdated = new String[ArrayNum]; //Array Holding Each reminder by version
-
+String priority = "null"; 
 
 
 
 Scanner keyboard = new Scanner(System.in);
 
-
 LocalDate myDateObj = LocalDate.now();
 //System.out.println("Before formatting: " + myDateObj);
-
 
 DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy");
 String formattedDate = myDateObj.format(myFormatObj);
@@ -48,6 +47,10 @@ EventNum = Integer.parseInt(InputNum);
 
 
 for(int i = 0; EventNum > i; EventNum--){
+
+		EventIncr++;
+		System.out.println("Enter the priority for your event " + EventIncr  + " (high med low): ");
+		priority = keyboard.nextLine();
 	
 		System.out.println("Enter Your Event date (use format yyyy-mm-dd) : ");
 		inputString = keyboard.nextLine();
@@ -65,25 +68,33 @@ for(int i = 0; EventNum > i; EventNum--){
 
 
 		LocalDate localDateR = LocalDate.now();
-
-		System.out.println("Enter in how many days you need your reminder:  : ");
+		
+		if (priority.equalsIgnoreCase("high") || priority.equalsIgnoreCase("med")) {
+		System.out.println("Enter in how many days you need your reminder: ");
 		Inputdays = keyboard.nextLine();
 		Days = Integer.parseInt(Inputdays);
-
+		}
+		else {}
 
 
 	if(Days <= 6) { //Days
-
+		
+		if(priority.equalsIgnoreCase("high") || priority.equalsIgnoreCase("med")) {
 		ReminderD = localDateR.plusDays(Days);
 		System.out.println("Enter Your reminder notification message: ");
 		ReminderDin = keyboard.nextLine();
 		System.out.println( "Setting reminder for "+ Titlein + " Event located at " + Localbot + " on "  + ReminderD);
 		//System.out.println("Reminder set for: "+ Days + " Days " + "\n"+ "The Date of your reminder is: " + ReminderD);
+		}
+		else {
+			System.out.println("Event Created.");
+		}
 
 }
 
 	else if(Days >= 7 && Days <= 29 ) { //Weeks
 
+		if(priority.equalsIgnoreCase("high") || priority.equalsIgnoreCase("med")) {
 		System.out.println("Enter Your reminder notification message: ");
 		ReminderWin = keyboard.nextLine();
 		Modulo = Days%7;
@@ -92,11 +103,16 @@ for(int i = 0; EventNum > i; EventNum--){
 		ReminderW = localDateR.plusWeeks(Days).plusDays(Modulo);
 		System.out.println( "Setting reminder for "+ Titlein + " Event located at " + Localbot + " on "  + ReminderW);
 		//System.out.println( "The Date of your reminder is: " + ReminderW);
+		}
+		else {
+			System.out.println("Event Created.");
+		}
 
 }
 
 	else if(Days >= 30 && Days <= 364) {  //Months
 
+		if(priority.equalsIgnoreCase("high") || priority.equalsIgnoreCase("med")) {
 		System.out.println("Enter Your reminder notification message: ");
 		ReminderMin = keyboard.nextLine();
 		Modulo = Days%30;
@@ -104,11 +120,16 @@ for(int i = 0; EventNum > i; EventNum--){
 		System.out.println("Reminder set for: " + Days + " Months " + " And " + Modulo + " Days ");
 		ReminderM= localDateR.plusMonths(Days).plusDays(Modulo);
 		System.out.println( "Setting reminder for "+ Titlein + " Event located at " + Localbot + " on "  + ReminderM);
+		}
+		else {
+			System.out.println("Event Created.");
+		}
 
 }
 
 	else if(Days >= 365) { //Years
 
+		if(priority.equalsIgnoreCase("high") || priority.equalsIgnoreCase("med")) {
 		System.out.println("Enter Your reminder notification message: ");
 		ReminderYin = keyboard.nextLine();
 		Modulo = Days%365;
@@ -117,15 +138,19 @@ for(int i = 0; EventNum > i; EventNum--){
 		ReminderY = localDateR.plusYears(Days).plusDays(Modulo);
 		System.out.println( "Setting reminder for "+ Titlein + " Event located at " + Localbot + " on "  + ReminderY);
 		//System.out.println( "The Date of your reminder is: " + ReminderY);
+		}
+		else {
+			System.out.println("Event Created.");
+		}
 
 }
 }
 
 
 version++;
-EventUpdated[version] = "\n"+ Event + "\n" + Titlein + "\n" + Localbot + "v" + version;
+EventUpdated[version] = "\n"+ Event + "\n" + Titlein + "\n" + Localbot + "v" + version;  //Saving Event Title and location in array with version
 
-if (ReminderDin != "null") {
+if (ReminderDin != "null") {                                                            //saving reminder in its own array with version
 	version2++;
 	ReminderUpdated[version2] = ReminderDin + "v" + version2;
 	
@@ -147,7 +172,7 @@ else if (ReminderYin != "null") {
 }
 }
 
-System.out.println(Arrays.toString(EventUpdated));
+System.out.println(Arrays.toString(ReminderUpdated));  //Test
 keyboard.close();
 }
 
